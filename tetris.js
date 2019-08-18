fb8ddc9b1073d37dbae1244c6e476181f34bd42a
Bryce Eadie <bryce.eadie@gmail.com>
2019-08-17 20:11:28 -0700

    Develop Tetris logic and actions

--- tetris.js
+++ tetris.js
@@ -3,11 +3,21 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20) // makes piece bigger
 
-const matrix = [
-  [0, 0, 0],
-  [1, 1, 1],
-  [0, 1, 0]
-];
+function arenaSweep() {
+  // 'continue' statement in 2nd for loop brings back to here
+  outer: for(let y = arena.length - 1; y > 0; --y) {
+    for(let x = 0; x < arena[y].length; ++x) {
+      if(arena[y][x] === 0) {
+        continue outer;
+      }
+    }
+
+    const row = arena.splice(y, 1)[0].fill(0)
+    // put cleared row back on top of arena
+    arena.unshift(row)
+    ++y
+  }
+}
 
 // tuple assigner?
 function collide(arena, player) {
@@ -33,6 +43,52 @@ function createMatrix(w, h) {
   return matrix;
 }
 
+function createPiece(type) {
+  if(type === 'T') {
+    return [
+      [0, 0, 0],
+      [1, 1, 1],
+      [0, 1, 0]
+    ];
+  } else if(type === 'O') {
+    return [
+      [2, 2],
+      [2, 2]
+    ];
+  } else if(type === 'L') {
+    return [
+      [0, 3, 0],
+      [0, 3, 0],
+      [0, 3, 3]
+    ];
+  } else if(type === 'J') {
+    return [
+      [0, 4, 0],
+      [0, 4, 0],
+      [4, 4, 0]
+    ];
+  } else if(type === 'I') {
+    return [
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0]
+    ];
+  } else if(type === 'S') {
+    return [
+      [0, 6, 6],
+      [6, 6, 0],
+      [0, 0, 0]
+    ];
+  } else if(type === 'Z') {
+    return [
+      [7, 7, 0],
+      [0, 7, 7],
+      [0, 0, 0]
+    ];
+  }
+}
+
 function draw() {
   context.fillStyle = '#000';
   context.fillRect(0, 0, canvas.width, canvas.height);
@@ -45,7 +101,7 @@ function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if(value !== 0) {
-        context.fillStyle = 'red'
+        context.fillStyle = colors[value]
         context.fillRect(x + offset.x,
                          y + offset.y,
                          1, 1);
@@ -69,7 +125,8 @@ function playerDrop() {
   if(collide(arena, player)) {
     player.pos.y--;
     merge(arena, player);
-    player.pos.y = 0;
+    playerReset()
+    arenaSweep()
   }
   dropCounter = 0;
 }
@@ -78,6 +135,55 @@ function playerMove(dir) {
   player.pos.x += dir;
   if(collide(arena, player)) {
     player.pos.x -= dir;
+  }
+}
+
+function playerReset() {
+  const pieces = 'ILJOTSZ'
+  //  ' | 0' = floors value?
+  player.matrix = createPiece(pieces[pieces.length * Math.random() | 0])
+  player.pos.y = 0
+  player.pos.x = (arena[0].length / 2 | 0) -
+                (player.matrix[0].length / 2 | 0);
+  // if piece immediately collides
+  if(collide(arena, player)) {
+    arena.forEach((row) => row.fill(0))
+  }
+}
+
+function playerRotate(dir) {
+  const pos = player.pos.x
+  let offset = 1
+  rotate(player.matrix, dir)
+  while(collide(arena, player)) {
+    player.pos.x += offset
+    offset = -(offset + (offset > 0 ? 1 : -1))
+    if(offset > player.matrix[0].length) {
+      rotate(player.matrix, -dir)
+      player.pos.x = pos
+      return
+    }
+  }
+}
+
+function rotate(matrix, dir) {
+  for(let y = 0; y < matrix.length; ++y) {
+    // tuple switch??
+    for(let x = 0; x < y; ++x) {
+      [
+        matrix[x][y],
+        matrix[y][x]
+      ] = [
+        matrix[y][x],
+        matrix[x][y]
+      ];
+    }
+  }
+
+  if(dir > 0) {
+    matrix.forEach((row) => row.reverse());
+  } else {
+    matrix.reverse();
   }
 }
 
@@ -98,11 +204,22 @@ function update(time = 0) {
   requestAnimationFrame(update)
 }
 
+const colors = [
+  null,
+  '#FF0D72',
+  '#0DC2FF',
+  '#0DFF72',
+  '#F538FF',
+  '#FF8E0D',
+  '#FFE138',
+  '#3877FF'
+]
+
 const arena = createMatrix(12, 20)
 
 const player = {
   pos: {x: 5, y: 5},
-  matrix: matrix
+  matrix: createPiece('T')
 }
 
 document.addEventListener('keydown', (event) => {
@@ -112,6 +229,10 @@ document.addEventListener('keydown', (event) => {
     playerMove(1)
   } else if(event.keyCode === 40) {
     playerDrop()
+  } else if(event.keyCode === 81) {
+    playerRotate(-1)
+  } else if(event.keyCode === 87) {
+    playerRotate(1)
   }
 })
 
